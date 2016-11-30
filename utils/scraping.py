e8f3cb5523dc524c0bbdeac18fdd479a3ef37d9d
Michel Llorens <mllorens@dcc.uchile.cl>
2016-11-30 11:01:00 -0300

    Refactor: Add method to get all major characters as list of tuples

--- utils/scraping.py
+++ utils/scraping.py
@@ -12,6 +12,8 @@ __license__ = "GPL"
 __version__ = "1.0.0"
 __email__ = "mllorens@dcc.uchile.cl"
 
+BASE_URL = 'http://wowwiki.wikia.com'
+
 
 def camel_case_split(identifier):
     matches = re.finditer('.+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)', identifier)
@@ -58,3 +60,21 @@ def parse_page(url_page):
         summary[subject] = answer
 
     return summary
+
+
+def get_major_characters():
+    base_url = BASE_URL
+    page = urllib2.urlopen("http://wowwiki.wikia.com/wiki/Category:Major_characters")
+    entity = BeautifulSoup(page, 'html.parser')
+    columns = entity.find('table').find_all('td')
+
+    return_list = list()
+
+    for col in columns:
+        urls = col.find_all('a', href=True)
+        for url in urls:
+            if url.text == u'Major characters':
+                continue
+            return_list.append((base_url + url['href'], url.text))
+
+    return return_list
