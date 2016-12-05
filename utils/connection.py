a89464f877dc6d6d5258d3224f760b2dc55ffcf4
Michel Llorens <mllorens@dcc.uchile.cl>
2016-12-05 11:35:03 -0300

    Update on scraper; Update requirements; Creation of connection to server

---
+++ utils/connection.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+
+__author__ = "Michel Llorens"
+__license__ = "GPL"
+__version__ = "1.0.0"
+__email__ = "mllorens@dcc.uchile.cl"
+
+UPDATE_URL = "http://127.0.0.1:3030/Warcraft/update"
+update_sparql = SPARQLWrapper(UPDATE_URL)
+
+QUERY_URL = "http://127.0.0.1:3030/Warcraft/query"
+query_sparql = SPARQLWrapper(QUERY_URL)
+
+
+def update_sparql_server(data):
+    update_sparql.resetQuery()
+    update_sparql.method = 'POST'
+    update_sparql.setQuery(data)
+    update_sparql.query()
+
+
+def query_sparql_server(query):
+    query_sparql.resetQuery()
+    query_sparql.setQuery(query)
+    query_sparql.setReturnFormat(JSON)
+    results = query_sparql.query().convert()
+    return results
