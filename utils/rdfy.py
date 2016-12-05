f3f60a34208afc6ee4a1f7b32c8de0f3ca4bee52
Tomas Saez B <tomas.saezbin@gmail.com>
2016-12-05 12:00:23 -0300

    modifying rdfy to create model based on new design . added image, warcraft prefix, url & nick

--- utils/rdfy.py
+++ utils/rdfy.py
@@ -4,30 +4,43 @@ from utils.scraping import *
 def scrap_to_rdf(url, data):
 
 
+    final_insert = list()
 
-    url_string = "<" + url + ">"
-    print url_string
+    #generar entidad  y agregarle nombre
     name_string = data['name']
-    print name_string
+    entity_string = "warcraft:" + name_string.replace(" ", "_")
+    name_string = "\"" + name_string + "\""
+    final_insert.append(entity_string + ' foaf:name ' + name_string + " .")
+    final_insert.append(entity_string + ' a warcraft:Character .' )
 
-    url_insert = url_string +' foaf:name '
-    url_insert = url_insert + "\"" + name_string + "\""
-    url_insert = url_insert + ' .\n'
-    print url_insert
+    #buscar url y agregarlo
+    url_string = "<" + url + ">"
+    final_insert.append(entity_string + ' foaf:isPrimaryTopicOf ' + url_string + ' .')
 
+    #buscar alias y agregarlo
     alias_string = data['alias']
-    print alias_string
+    final_insert.append(entity_string + ' foaf:nick ' + "\"" + alias_string + "\"" + ' .')
+
+    #buscar genero y agregarlo
     gender_string = data['Gender']
-    print gender_string
+    final_insert.append(entity_string + ' foaf:gender ' + "\"" + gender_string + "\"" + ' .')
+
+    #buscar imagen y agregarla
+    image_string = data['image']
+    final_insert.append(entity_string + ' foaf:img ' + "<" + image_string + ">" + ' .')
 
 
 
+
+    #juntando todas los triples creados
+    final_insert = "\n".join(final_insert)
 
     a = """PREFIX foaf:  <http://xmlns.com/foaf/0.1/>
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema>
-    INSERT DATA {
-    %s
-    }; """ % (url_insert)
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema>
+PREFIX warcraft: <http://www.warcraft.rdf/>
+INSERT DATA {
+%s
+}; """ % (final_insert)
     print a
 
 
