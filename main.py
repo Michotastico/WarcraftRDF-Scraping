2afb4f6241d51c949548318cefadb70c770e89fc
Michel Llorens <mllorens@dcc.uchile.cl>
2016-12-12 12:51:15 -0300

    Update on main

--- main.py
+++ main.py
@@ -4,18 +4,21 @@ from utils.connection import *
 
 to_do_chars = get_major_characters()
 done_chars = {}
-
+already_counter = 0
 while to_do_chars:
     scraping_char = to_do_chars.pop(0)
 
+    print already_counter, scraping_char
+    already_counter += 1
 
-    print scraping_char
     if not done_chars.get(scraping_char[1], False):
 
         script, new_chars = scrap_to_rdf(scraping_char[0], parse_page(scraping_char[0]))
+        try:
+            update_sparql_server(script)
 
-        update_sparql_server(script)
+            to_do_chars.extend(new_chars)
 
-        to_do_chars.extend(new_chars)
-
-        done_chars[scraping_char[1]] = scraping_char[0]
+            done_chars[scraping_char[1]] = scraping_char[0]
+        except:
+            print script
